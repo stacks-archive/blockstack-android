15d04ae79fa7613d4c47f306a54274bd0365e11c
Friedger Müffke <mail@friedger.de>
2019-01-18 12:59:19 +0100

    fix #116, add documentation

--- blockstack-sdk/src/main/java/org/blockstack/android/sdk/SessionStore.kt
+++ blockstack-sdk/src/main/java/org/blockstack/android/sdk/SessionStore.kt
@@ -10,25 +10,30 @@ val BLOCKSTACK_SESSION = "blockstack_session"
 private val EMPTY_DATA = "{}"
 private val TAG = SessionStore::class.java.simpleName
 
-
+/**
+ * Interface giving access to stored Blockstack session data.
+ */
 interface ISessionStore {
     var sessionData: SessionData
     fun deleteSessionData()
 }
 
+/**
+ * Object giving access to stored Blockstack session data. The data is stored in
+ * the provided preferences.
+ *
+ * @param prefs shared preferences used to store the data.
+ */
 class SessionStore(private val prefs: SharedPreferences) : ISessionStore {
-    var sessionDataObject = SessionData(JSONObject(prefs.getString(BLOCKSTACK_SESSION, EMPTY_DATA)))
     override var sessionData: SessionData
-        get() = sessionDataObject
+        get() {
+            return SessionData(JSONObject(prefs.getString(BLOCKSTACK_SESSION, EMPTY_DATA)))
+        }
         set(value) {
-            Log.d(TAG, "set session data in store " + value.json.toString())
-            sessionDataObject = value
             prefs.edit().putString(BLOCKSTACK_SESSION, value.json.toString()).apply()
         }
 
     override fun deleteSessionData() {
         prefs.edit().putString(BLOCKSTACK_SESSION, EMPTY_DATA).apply()
-        sessionDataObject = SessionData(JSONObject())
     }
-
 }
